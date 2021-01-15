1d8af08e45c259d903eca059b8955add04926674
Rohan Julka <rohanjulka19@gmail.com>
2021-01-15 18:32:20 +0530

    Added User POJO

---
+++ src/main/java/com/app/bookmymovie/pojo/User.java
@@ -0,0 +1,66 @@
+package com.app.bookmymovie.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	int id ;
+	String name ;
+	String email; 
+	String password ;
+	String phone_no ;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(int id, String name, String email, String password, String phone_no) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.phone_no = phone_no;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone_no() {
+		return phone_no;
+	}
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", phone_no="
+				+ phone_no + "]";
+	}
+
+}
